7d18404ec50891837f35c27aa153b29b0549db08
P0Vlin <p0vlinrei@gmail.com>
2023-12-18 20:38:41 +0500

    Tried to incapsulate

--- src/main/java/ru/fatum/tgbot/telegram/TelegramBot.java
+++ src/main/java/ru/fatum/tgbot/telegram/TelegramBot.java
@@ -16,6 +16,7 @@ import ru.fatum.tgbot.config.BotConfig;
 import java.util.ArrayList;
 import java.util.List;
 import ru.fatum.tgbot.tglogic.KeyboardUtils;
+import ru.fatum.tgbot.logic.MessageHandler;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
@@ -30,8 +31,10 @@ public class TelegramBot extends TelegramLongPollingBot {
     private boolean isBotEnabled = false;
     final BotConfig config;
     static final String HELP_TEXT = "help text lol";
+    private final MessageHandler messageHandler;
 
     public TelegramBot(BotConfig config) throws TelegramApiException {
+        this.messageHandler = new MessageHandler(this);
         this.config = config;
         List <BotCommand> listofCommands = new ArrayList<>();
         listofCommands.add(new BotCommand("/start", "launch the bot"));
@@ -42,77 +45,7 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-
-        if(update.hasMessage() && update.getMessage().hasText()) {
-            String messageText = update.getMessage().getText();
-            long chatId = update.getMessage().getChatId();
-
-            if (messageText.startsWith("/start")) {
-                isBotEnabled = true;
-                currentState = BotState.REGULAR;
-            }
-
-            if (!isBotEnabled){
-                return;
-            }
-
-            if (messageText.startsWith("/help")) {
-                KeyboardUtils.sendMessage(this,chatId, HELP_TEXT);
-            }
-
-            switch (currentState) {
-                /*case MEMORGET:
-                    if (messageText.startsWith("Retain Memory")) {
-                        sendResponseWithKeyboard(chatId, "Would you like to take a test to determine your current mood?", getKeyboardForAskTest());
-                        currentState = BotState.ASKTOTAKETEST;
-                        break;
-                    }
-                    else if (messageText.startsWith("Recall Memories")) {
-                        currentState = BotState.GIVEMEMSTORAGE;
-                        break;
-                    }
-                    else{
-                        sendResponseWithKeyboard(chatId, "What do you want to do?" , getKeyboardForDef());
-                        currentState = BotState.MEMORGET;
-                        break;
-                    }*/
-
-                case ASKTOTAKETEST:
-                    if(messageText.startsWith("Yes | Take Test")){
-                        KeyboardUtils.sendResponseWithKeyboard(this,chatId, "To Be or Not To Be?", KeyboardUtils.getKeyboardFor1q());
-                        currentState = BotState.qFIRST;
-                        break;
-                    }
-                    //ЕЩЁ ОДИН ИФ
-                    else {
-                        KeyboardUtils.sendResponseWithKeyboard(this,chatId, "Would you like to take a test to determine your current mood?", KeyboardUtils.getKeyboardForAskTest());
-                        return;
-                    }
-
-                case REGULAR:
-                    if (messageText.startsWith("/savememory")){
-                        KeyboardUtils.sendResponseWithKeyboard(this,chatId, "Would you like to take a test to determine your current mood?", KeyboardUtils.getKeyboardForAskTest());
-                        currentState = BotState.ASKTOTAKETEST;
-                        break;
-                    }
-                    if (messageText.startsWith("/memories")){
-                        //Возможно в будующем можно будет сделать чтобы бот предлагал воспоминания по годам, когда их станет очень много, только как это сделать динамически я пока что не знаю
-                        currentState = BotState.GIVEMEMSTORAGE;
-                        break;
-                    }
-                    else{
-                        KeyboardUtils.sendMessage(this,chatId,"This bot can help you to save your beautiful memories \n" +
-                                "To control this bot you can use these commands:\n" +
-                                "/savememory - to remember something important for you\n" +
-                                "/memories - get your saved memories" );
-                        return;
-                    }
-
-                default:
-                    KeyboardUtils.sendMessage(this,chatId, "Please choose one of the options");
-                    break;
-            }
-        }
+        messageHandler.handleUpdate(update);
     }
     @Override
     public String getBotUsername() {
